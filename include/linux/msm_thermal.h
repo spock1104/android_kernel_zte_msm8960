185e2c2c841b5ca35270b09ad707480641e2ea75
spock1104 <spock1104@gmail.com>
2015-03-29 01:17:49 -0400

    add showp1984 thermal controls

--- include/linux/msm_thermal.h
+++ include/linux/msm_thermal.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2012, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -14,12 +14,33 @@
 #ifndef __MSM_THERMAL_H
 #define __MSM_THERMAL_H
 
+#include <asm/cputime.h>
+
 struct msm_thermal_data {
 	uint32_t sensor_id;
 	uint32_t poll_ms;
-	uint32_t limit_temp_degC;
-	uint32_t temp_hysteresis_degC;
-	uint32_t freq_step;
+	uint32_t shutdown_temp;
+
+	uint32_t allowed_max_high;
+	uint32_t allowed_max_low;
+	uint32_t allowed_max_freq;
+
+	uint32_t allowed_mid_high;
+	uint32_t allowed_mid_low;
+	uint32_t allowed_mid_freq;
+
+	uint32_t allowed_low_high;
+	uint32_t allowed_low_low;
+	uint32_t allowed_low_freq;
+};
+
+struct msm_thermal_stat {
+    cputime64_t time_low_start;
+    cputime64_t time_mid_start;
+    cputime64_t time_max_start;
+    cputime64_t time_low;
+    cputime64_t time_mid;
+    cputime64_t time_max;
 };
 
 #ifdef CONFIG_THERMAL_MONITOR
@@ -37,3 +58,4 @@ static inline int msm_thermal_device_init(void)
 #endif
 
 #endif /*__MSM_THERMAL_H*/
+
